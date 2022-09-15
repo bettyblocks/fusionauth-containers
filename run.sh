867c05a1669ce90ae91afc6923ce368ed221b945
Stephan Kaag <stephan@ka.ag>
2022-09-15 17:17:43 +0200

    chore: use cmd instead of entrypoint

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-$CERT = "/etc/ssl/certs/domain-ca-cert.crt"
+CERT="/etc/ssl/certs/domain-ca-cert.crt"
 
 if [ -f "$CERT" ]; then
     /opt/openjdk/bin/keytool -importcert -file $CERT -keystore fusionauth/java/current/lib/security/cacerts -storepass changeit  -alias faselfsignedcert
 fi
 
-# Hand off to the CMD
-exec "$@"
+/usr/local/fusionauth/fusionauth-app/apache-tomcat/bin/catalina.sh run
