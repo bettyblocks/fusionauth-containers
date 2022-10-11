a9fbfa96b3ed6a1c6445b665aecdd8f30f0bb30a
Stephan Kaag <stephan@ka.ag>
2022-10-11 19:29:08 +0200

    fix: adjust  args again

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 CERT="/etc/ssl/certs/domain-ca-cert.crt"
 
 if [ -f "$CERT" ]; then
-    /opt/openjdk/bin/keytool -importcert -file $CERT -keystore /tmp/cacerts -storepass changeit  -alias faselfsignedcert -noprompt
+    /opt/openjdk/bin/keytool -importcert -file $CERT -cacerts -storepass changeit -alias faselfsignedcert -noprompt -trustcacerts
 fi
 
 /usr/local/fusionauth/fusionauth-app/apache-tomcat/bin/catalina.sh run
