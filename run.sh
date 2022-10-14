f7c42ddfc2a665765d59efc9a3a5211363e5f6ba
Stephan Kaag <stephan@ka.ag>
2022-10-14 12:39:30 +0200

    feat: add multiple certs

--- run.sh
+++ run.sh
@@ -3,7 +3,19 @@
 CERT="/etc/ssl/certs/domain-ca-cert.crt"
 
 if [ -f "$CERT" ]; then
-    /opt/openjdk/bin/keytool -importcert -file $CERT -cacerts -storepass changeit -alias faselfsignedcert -noprompt -trustcacerts
+    # number of certs in the PEM file
+    CERTS=$(grep 'END CERTIFICATE' $CERT| wc -l)
+
+    # For every cert in the PEM file, extract it and import into the JKS keystore
+    # awk command: step 1, if line is in the desired cert, print the line
+    #              step 2, increment counter when last line of cert is found
+    for N in $(seq 0 $(($CERTS - 1))); do
+    ALIAS="${CERT%.*}-$N"
+    cat $CERT |
+        awk "n==$N { print }; /END CERTIFICATE/ { n++ }" |
+        /opt/openjdk/bin/keytool -import -cacerts -trustcacerts \
+                -alias $ALIAS -storepass changeit -noprompt
+    done    
 fi
 
 /usr/local/fusionauth/fusionauth-app/apache-tomcat/bin/catalina.sh run
